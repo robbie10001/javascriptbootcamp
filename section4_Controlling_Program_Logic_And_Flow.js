bfbdd7f541eaaf8232a726e15ae09103c9ec55e2
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-04 19:40:57 +1100

    Java Script Operators

---
+++ section4_Controlling_Program_Logic_And_Flow.js
@@ -0,0 +1,53 @@
+
+/*
+GOALS OF THIS SECTION 
+
+understanding comparison operators
+write conditional statements.
+work with boolean operators.
+
+*/ 
+/*
+COMPARISON OPERATORS.
+
+> greater than
+> less than
+>= greater than or equal to 
+<= less than or equal to 
+== equality 
+!= not equal
+=== strict equality 
+!== stict non-equality 
+*/ 
+/*
+DOUBLE EQUALS == 
+
+== (equality operator)
+
+the equality operator checks for equality of value, but not equality of type.
+It coerces both values to the same type and then compares them. 
+This can lead to some unexpected results!
+*/ 
+/*
+TRIPLE EQUALS ===
+
+=== (strict equality operator)
+
+Checks for EQUALITY of VALUE AND TYPE! 
+It is more precise and specific than the equality operator.
+FOR THE VAST MAJORITY OF TIME, WE SHOULD USE TRIPLE EQUALS! 
+
+*/ 
+/*
+NON EQUALITY (!=) AND NON-STRICT EQUALITY (!==)
+
+For the same reason we use the strict equality operator,
+we should use the strict non-equality operator (!==)
+
+*/ 
+/*
+
+
+
+
+*/
