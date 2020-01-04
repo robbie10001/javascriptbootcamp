b6aa787d86ccfcf7d67c27dba78cdcf3fe823934
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-04 20:11:50 +1100

    finished conditional statements

--- section4_Controlling_Program_Logic_And_Flow.js
+++ section4_Controlling_Program_Logic_And_Flow.js
@@ -1,3 +1,4 @@
+//alert("its working!");
 
 /*
 GOALS OF THIS SECTION 
@@ -46,8 +47,75 @@ we should use the strict non-equality operator (!==)
 
 */ 
 /*
+CONDITIONAL STATEMENTS 
 
+IF STATEMENTS
 
+IF - Runs code if a given condition is true 
+*/
+//example 1 
+if (1 === 1){
+    console.log("It's True");
+};
 
+//This if statement would be evaluated to true. 
+
+let rating = 3; 
+//example 2 
+if(rating === 3) {
+    console.log("You are a superstar!");
+}
+//example 3 
+if(rating < 3) {
+    console.log("You are not a superstar!");
+}
+// example 4 
+let num = 37; 
+if(num % 2 !== 0){
+    console.log("ODD NUMBER!");
+}
+
+/*
+ELSE IF STATEMENTS AND ELSE STATEMENTS 
+
+If not the first thing, maybe this other thing? 
 
 */
+
+//example 1 
+
+//Peformance Review 
+//3 - superstar
+//2 - meets expectations
+//1 - needs improvement 
+// - anything else wtf? 
+
+let employerRating = 2; 
+
+if(employerRating === 3){
+    console.log("You are a superstar!");
+}
+else if(employerRating === 2) {
+    console.log("meets expectations")
+}
+else if (employerRating === 1) {
+    console.log("Needs improvement!")
+}
+else {
+    console.log("Not a valid rating!")
+};
+
+//While if and else both require a condition to be put within parentheses(), with an else statement this is not the case. 
+
+//EXAMPLE 2 
+
+let highScore = 1430; 
+let userScore = 1200; 
+
+if(userScore > highScore) {
+    console.log(`Congrads, you have the new high score of ${userScore}`);
+    highScore = userScore;
+}
+else {
+    console.log(`Good Game, your score of ${userScore} did not beat the high score of ${highScore}`)
+}
