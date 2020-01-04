ba7357e6163a64708d6363cadbfab95071be1f08
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-04 11:30:16 +1100

    JS, ECMA, TC39: What do they mean?

---
+++ section1_introduction/index.js
@@ -0,0 +1,13 @@
+/*
+JS, ECMA, TC39: WHAT DO THEY MEAN? 
+
+ECMA script is a specification for a programming language. Its a document that describes how a language should work. 
+It is then up to the browser to implement these rules as Javascript. 
+
+TC39, is a technical committee that is in charge of maintaining and evolving the ECMAscript specifications.
+
+ES6  took almost 6 years to release. Many features were ready sooner but had to wait for the main release. After this, they decided to release changes yearly. 
+ 
+caniuse.com - is a good website to see if a feature of ECMAscript we are using is available in a given browser. 
+*/
+
