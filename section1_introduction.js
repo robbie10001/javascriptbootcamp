7ca8de2692daae5fc4a64c21544d1827b71e1659
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-04 11:52:34 +1100

    fixed file structure

--- section1_introduction.js
+++ section1_introduction.js
@@ -1,17 +1,13 @@
 /*
 JS, ECMA, TC39: WHAT DO THEY MEAN? 
-
 ECMA script is a specification for a programming language. Its a document that describes how a language should work. 
 It is then up to the browser to implement these rules as Javascript. 
-
 TC39, is a technical committee that is in charge of maintaining and evolving the ECMAscript specifications.
-
 ES6  took almost 6 years to release. Many features were ready sooner but had to wait for the main release. After this, they decided to release changes yearly. 
  
 caniuse.com - is a good website to see if a feature of ECMAscript we are using is available in a given browser. 
-
 While there is no an offical javascript we use, a good place o et information is 
-
 MDN! - This is a really good resource for Javascript developers. 
-*/
 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
+*/
