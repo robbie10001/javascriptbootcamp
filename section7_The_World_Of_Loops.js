912b834a93277afdb7b3f1b0be89e518f62bd2fb
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-07 07:15:19 +1100

    loop intro added

--- section7_The_World_Of_Loops.js
+++ section7_The_World_Of_Loops.js
@@ -1,6 +1,19 @@
 /*
+GOALS FOR THIS SECTION
+
+Write for loops 
+write while loops
+Avoid infinite loops
+Iterate over arrays and objects
+
+*/
+
+
+/*
 INTRO TO LOOPS 
 
+Looping is about how we repeat code. 
+There are multiple types of loops   for loop, while loop, for...of loop, for...in loop
 
 */
 
