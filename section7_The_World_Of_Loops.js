8ca3332baaa7c0611cf5462968b9ee7a55cfc8b9
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-07 07:35:29 +1100

    For Loops

--- section7_The_World_Of_Loops.js
+++ section7_The_World_Of_Loops.js
@@ -6,10 +6,6 @@ write while loops
 Avoid infinite loops
 Iterate over arrays and objects
 
-*/
-
-
-/*
 INTRO TO LOOPS 
 
 Looping is about how we repeat code. 
@@ -20,9 +16,36 @@ There are multiple types of loops   for loop, while loop, for...of loop, for...i
 /*
 FOR LOOPS 
 
+There are three seperate pieces to a FOR loop. 
+1.Initial value- The main idea is that we define a variable,
+2. When to run the loop- We then add a counter which acts to control the duration of the loops
+3. How to change value each iteration- What happens to the variable each time we go through the loop
+
+for ([initiaExpression]; [condition]; [incrementExpression]
+)
+
 
 */
+//let is just a variable we have defined within the loop
+//stop the loops at 10 
+//add i each time 
 
+for(let i = 1; i <= 10; i++){
+    console.log("Hello") //Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+};
+
+for(let i = 1; i <= 10; i+= 3){
+    console.log(i) //1, 4, 7, 10 
+};
+
+for(let num = 1; num <= 5; num++) {
+    console.log(`${num}x${num} = ${num * num}`); //1x1 = 1, 2x2 = 4, 3x3 = 9, 4x4 = 16, 5x5 = 25
+};
+//We can also loop in the other direction by counting backwards
+
+for(let i = 200; i>= 0; i-=25) {
+    console.log(i); //200, 175, 150, 125, 100, 75, 50, 25, 0 
+}
 /*
 INFINITE LOOPS 
 
