8dfea58e37076e6ef2e70c2a02cb7a3c6779494a
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-07 22:17:56 +1100

    first function

--- section8_Writing_Reusable_Code_With_Functions.js
+++ section8_Writing_Reusable_Code_With_Functions.js
@@ -1,12 +1,59 @@
 /*
-OUR FIRST FUNCTION 
-
-
-
+GOALS FOR THIS SECTION
+Write Valid Functions
+Write functions with arguments 
+compare function expressions and function statements 
+write a bunch of functions! 
 */
 
+/*
+OUR FIRST FUNCTION 
 
+Functions are really important in order to be a Javascript developer. 
+Functions are very important to JavaScripts identity as a language. 
+There are lots of things we can do with functions that are really cool. 
+Writing functions are not difficult, but there are lots of things to understand about the features of functions.
 
+Functions are resuable procedures. It's a bunch of code that we wrap up in a function.
+It a bunch of lines of code that we can call again at any point. We can refer back to it. 
+We could have a function called getlatestredditpost, and everytime we called the function would request new reddit post and send them back to us. 
+Or we could have a function that gives us the average value from within an array.
+Functions allow us to wrtie resuable, modular code. 
+We define a chunk of code that we can then excute at a later point.
+We us them ALL THE TIME!
+
+There is a two step process for every functin that we ever write.
+1. DEFINE - we tell JavaScript this is how it works 
+2. RUN- We execute the function and run it! 
+
+We have used Methods before, and methods are functions! 
+ 
+For example, 
+
+"hello".toUpperCase(); 
+Is a build in function, it's already defined for us and we just use it! 
+
+Defining a function 
+
+function functionName () {
+    do something
+};
+*/
+//all this function does is print out the three console logs below. 
+//every time we call call a function these statements will be printed out. 
+function grumpus() {
+    console.log("You again");
+    console.log("for the last time!");
+    console.log('leave me alone!');
+}
+//in order to run the function, we have to call the function. 
+grumpus();//you again, for the last time, leave me alone! 
+//in order to run the code we need () parenthesis 
+//we can loop through our functions 
+
+for (let i = 0; i >3; i++) {
+    console.log(grumpus());
+}
 /*
 DICE ROLL FUNCTION
 
