aaeafa30f130bde44cab35deb433cc539a39bf50
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-07 21:14:56 +1100

    function boilerplate added

---
+++ section8_Writing_Reusable_Code_With_Functions.js
@@ -0,0 +1,87 @@
+/*
+OUR FIRST FUNCTION 
+
+
+
+*/
+
+
+
+/*
+DICE ROLL FUNCTION
+
+
+
+*/
+
+
+
+/*
+INTRODUCING ARGUMENTS
+
+
+
+*/
+
+
+
+/*
+FUNCTIONS WITH MULTIPLE ARGUMENTS
+
+
+
+*/
+
+
+
+/*
+THE RETURN STATEMENT
+
+
+
+*/
+
+
+
+/*
+MORE ON RETURN VALUES 
+
+
+
+*/
+
+
+
+/*
+FUNCTION CHALLENGE 1: PASSWORDVALIDATOR
+
+
+
+*/
+
+
+
+/*
+FUNCTION CHALLENGE2: AVERAGE
+
+
+
+*/
+
+
+
+/*
+FUNCTION CHALLENGE 3: PANGRAMS
+
+
+
+*/
+
+
+
+/*
+FUNCTION CHALLENGE 4: GET PLAYING CARDS
+
+
+
+*/
